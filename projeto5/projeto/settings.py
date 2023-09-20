d11627ce2c9e5aba31e2505b337602b8672eef48
pedrolisboaa <phlisboa2000@gmail.com>
2023-09-19 22:08:08 -0300

    finalizando capitulo

--- projeto5/projeto/settings.py
+++ projeto5/projeto/settings.py
@@ -132,3 +132,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'usuarios.CustomUsuario'
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
